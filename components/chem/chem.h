5d1d72e87cbbdb43a3c8f3ae32cdb429bac1de2d
vaca <wsmslgh@gmail.com>
2022-10-22 00:47:17 -0700

    Initial commit

---
+++ components/chem/chem.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <stdlib.h>
+#include <string.h>
+#include <vector>
+#include <string>
+#include "rationalMatrix.h"
+using namespace std;
+class ChemItem
+{
+public:
+    int type;
+    int num;
+    ChemItem(int myType, int myNum);
+    bool operator==(const ChemItem &b);
+};
+
+class ChemEquation
+{
+public:
+       ChemEquation(const char ga[]);
+        ~ChemEquation();
+        string myResult();
+
+private:
+    int myRow;
+    int myColume;
+    RationalMatrix *matrix;
+    void setChem(const char *chem);
+    void toMatrixBaby();
+
+    const ChemItem N1 = ChemItem(0, 0);
+    const ChemItem N2 = ChemItem(-1, 0);
+    char *chemMine;
+    int chemMineLen;
+    string chemString;
+    vector<string> cutString;
+    vector<string> elementList;
+    vector<ChemItem> complexResult;
+    int **baby;
+
+    int convertNumber(int k);
+    void toList();
+    int findEle(string x);
+    int signChem(int x);
+};
---
+++ main/chem1.cpp
@@ -0,0 +1,131 @@
+#include "esp_log.h"
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "chem.h"
+#include <string>
+#include "esp_task_wdt.h"
+#include "esp32_perfmon.h"
+#include "freertos/semphr.h"
+#include "soc/timer_group_struct.h"
+#include "driver/periph_ctrl.h"
+#include "driver/timer.h"
+#include "esp_timer.h"
+#include "freertos/task.h"
+#include <iostream>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+
+
+static const char *TAG = "handle";
+#define NUM 100000
+
+
+
+extern "C"
+{
+    void app_main();
+}
+int main();
+void app_main(void)
+{
+    main();
+
+}
+
+
+
+
+
+typedef struct {
+    float x;
+    float y;
+} Point;
+
+float fRand(float fMin, float fMax) {
+    float f = (float) rand() / RAND_MAX;
+    return fMin + f * (fMax - fMin);
+}
+
+Point pRand() {
+    float x, y;
+    Point p;
+    do {
+        x = fRand(-1, 1);
+        y = fRand(-1, 1);
+    } while (x * x + y * y < 1);
+    p.x = x;
+    p.y = y;
+    return p;
+}
+
+float cross_3(Point a, Point b) {
+    return a.x * b.y - b.x * a.y;
+}
+
+bool isSameSign(float *n_array, int len) {
+    for (int k = 0; k < len; k++) {
+        for (int j = k; j < len; j++) {
+            if (n_array[k] * n_array[j] < 0) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+
+
+bool isInSemiCircle(Point *p_array, int len) {
+    float *c3 = static_cast<float *>(malloc((len - 1) * sizeof(float)));
+
+    for(int j=0;j<len;j++){
+        int index=0;
+        for (int k = 0; k < len; k++) {
+            if(k!=j){
+                c3[index] = cross_3(p_array[j], p_array[k]);
+                index++;
+            }
+        }
+        bool result = isSameSign(c3, len - 1);
+        if(result){
+            free(c3);
+            return true;
+        }
+    }
+
+    free(c3);
+    return false;
+}
+
+
+
+int main() {
+    auto start = high_resolution_clock::now();
+
+    int s = 0;
+    int p_num = 4;
+
+    srand(time(NULL));
+
+    Point *p_array = static_cast<Point *>(malloc(p_num * sizeof(Point)));
+    for (int k = 0; k < NUM; k++) {
+        for (int j = 0; j < p_num; j++) {
+            p_array[j] = pRand();
+        }
+        if (isInSemiCircle(p_array, p_num)) {
+            s++;
+        }
+    }
+    float p = (float) s / NUM;
+    std::cout << "probability: " << p << std::endl;
+    free(p_array);
+
+
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << "Time taken by function: "
+         << duration.count() << " microseconds" << endl;
+    return 0;
+}
